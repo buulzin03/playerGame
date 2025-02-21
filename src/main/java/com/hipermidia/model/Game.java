c70e9b86e78f9a190fe2a3aa35a3f9ccd9294c10
buulzin03 <jet.power03@gmail.com>
2025-02-21 19:17:55 -0300

    adding gson repository

--- src/main/java/com/hipermidia/model/Game.java
+++ src/main/java/com/hipermidia/model/Game.java
@@ -2,15 +2,21 @@ package com.hipermidia.model;
 
 import java.util.List;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Game {
     
     private String title;
     private String description;
     private String author;
     private Integer startLocationId;
+    @SerializedName("max_itens")
     private Integer maxItens;
+    @SerializedName("max_turns_easy")
     private Integer maxTurnsEasy;
+    @SerializedName("max_turns_normal")
     private Integer maxTurnsNormal;
+    @SerializedName("max_turns_hard")
     private Integer maxTurnsHard;
     private Double attack = 10.0;
     private Double defense = 8.0;
--- src/main/java/com/hipermidia/model/Item.java
+++ src/main/java/com/hipermidia/model/Item.java
@@ -1,9 +1,12 @@
 package com.hipermidia.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Item {
     private Integer id;
     private String name;
     private String description;
+    @SerializedName("can_take")
     private Boolean canTake = false;
     private Boolean inactive = false;
     
